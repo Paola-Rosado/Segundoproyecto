e68454b431c63e82d02bfb0fab79830bbce041e7
Paola Pech <ros.paolapech@gmail.com>
2026-03-15 19:56:41 -0600

    act

--- database/migrations/2026_03_10_223430_create_paginas_table.php
+++ database/migrations/2026_03_10_223430_create_paginas_table.php
@@ -9,7 +9,15 @@ return new class extends Migration
     {
         Schema::create('paginas', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
+            $table->string('name');
+            /**El siguiente campo debe ser único en la tabla */
+            $table->string('email')->unique();
+            //campo tipo fecha y hora que puede ser nulo
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('avatar')->nullable();
+            $table->string('telefono');
+            $table->string('calle');
             $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
